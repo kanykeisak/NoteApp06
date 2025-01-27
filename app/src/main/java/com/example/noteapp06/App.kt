f387e635da88e62c959cd2926fee9ad395fc4335
kanykeisak <isakova0116@gmail.com>
2025-01-27 14:23:03 +0600

    Homework 5

--- app/src/main/java/com/example/noteapp06/App.kt
+++ app/src/main/java/com/example/noteapp06/App.kt
@@ -30,6 +30,7 @@ class App: Application() {
                 ).fallbackToDestructiveMigration().allowMainThreadQueries().build()
             }
         }
+        
         return appDatabase
     }
 }
